595d19bd67d0f31bc2ab6699c1d8301fd529c403
Kevin <kevin31yu@gmail.com>
2024-06-19 10:51:42 -0700

    v 1.0

--- index.js
+++ index.js
@@ -1,55 +1,46 @@
 window.onload = function () {
     for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
         document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-            switch (this.innerHTML) {
-                case "w":
-                    new Audio("sounds/crash.mp3").play();
-                    break;
-                case "a":
-                    new Audio("sounds/snare.mp3").play();
-                    break;
-                case "s":
-                    new Audio("sounds/tom-1.mp3").play();
-                    break;
-                case "d":
-                    new Audio("sounds/tom-2.mp3").play();
-                    break;
-                case "j":
-                    new Audio("sounds/tom-3.mp3").play();
-                    break;
-                case "k":
-                    new Audio("sounds/tom-4.mp3").play();
-                    break;
-                default:
-                    new Audio("sounds/kick-bass.mp3").play();
-            }
+            handle(this.innerHTML);
+            animation(this.innerHTML);
         });
     }
 
-    for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
-        document.addEventListener("keydown", function (e) {
-            switch (e.key) {
-                case "w":
-                    new Audio("sounds/crash.mp3").play();
-                    break;
-                case "a":
-                    new Audio("sounds/snare.mp3").play();
-                    break;
-                case "s":
-                    new Audio("sounds/tom-1.mp3").play();
-                    break;
-                case "d":
-                    new Audio("sounds/tom-2.mp3").play();
-                    break;
-                case "j":
-                    new Audio("sounds/tom-3.mp3").play();
-                    break;
-                case "k":
-                    new Audio("sounds/tom-4.mp3").play();
-                    break;
-                default:
-                    new Audio("sounds/kick-bass.mp3").play();
-            }
-        });
+    document.addEventListener("keydown", function (e) {
+        handle(e.key);
+        animation(e.key);
+    });
+}
+
+function handle(key) {
+    switch (key) {
+        case "w":
+            new Audio("sounds/crash.mp3").play();
+            break;
+        case "a":
+            new Audio("sounds/snare.mp3").play();
+            break;
+        case "s":
+            new Audio("sounds/tom-1.mp3").play();
+            break;
+        case "d":
+            new Audio("sounds/tom-2.mp3").play();
+            break;
+        case "j":
+            new Audio("sounds/tom-3.mp3").play();
+            break;
+        case "k":
+            new Audio("sounds/tom-4.mp3").play();
+            break;
+        default:
+            new Audio("sounds/kick-bass.mp3").play();
     }
+}
+
+function animation(key) {
+    var activeButton = document.querySelector("." + key);
+    activeButton.classList.add("pressed");
+    setTimeout(function () {
+        activeButton.classList.remove("pressed")
+    }, 100);
 }
